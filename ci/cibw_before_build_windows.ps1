7f7274b8bd56cba59fec512310793aa3398e3dc7
Amelie <amelie.huebner@uni-jena.de>
2026-09-17 17:58:39 +0200

    yay windows

--- ci/cibw_before_build_windows.ps1
+++ ci/cibw_before_build_windows.ps1
@@ -47,10 +47,8 @@ cmd.exe /c "call `"$VcVars`" && set" | ForEach-Object {
         [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
     }
 }
-cl.exe
-if ($LASTEXITCODE -ne 0) {
-    throw "cl.exe failed with exit code $LASTEXITCODE"
-}
+Write-Host "MSVC:"
+cmd.exe /c "cl" 2>&1 | Select-String "Version"
 
 # get RDKit headers for bindings
 Set-Location "C:\"
